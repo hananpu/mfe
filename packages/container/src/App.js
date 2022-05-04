fb4fe19a6c5003da8b8f62448b9f8acd20b6ff7f
Caner <caner@favorisanat.com>
2022-05-04 15:10:24 +0300

    Master / createGenerateClassName added to StylesProvider

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
+  /**
+   * class name collision => Material ui productionda jss1, jss2 şeklinde classlar üretiyor. Diğer projelerde de material ui varsa onlarda da jss1, jss2
+   * şeklinde className üreteceğinden createGenerateClassName methodu ile classların prefixini ayarlıyoruz.
+   */
+  const generateClassName = createGenerateClassName({
+    productionPrefix: "ca",
+  });
   return (
     <BrowserRouter>
-      <Header />
-      <MarketingApp />
+      <StylesProvider generateClassName={generateClassName}>
+        <Header />
+        <MarketingApp />
+      </StylesProvider>
     </BrowserRouter>
   );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
 export default () => {
+  /**
+   * class name collision => Material ui productionda jss1, jss2 şeklinde classlar üretiyor. Diğer projelerde de material ui varsa onlarda da jss1, jss2
+   * şeklinde className üreteceğinden createGenerateClassName methodu ile classların prefixini ayarlıyoruz.
+   */
+  const generateClassName = createGenerateClassName({
+    productionPrefix: "ma",
+  });
+
   return (
-    <StylesProvider>
+    <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
         <Switch>
           <Route exact path="/pricing" component={Pricing} />

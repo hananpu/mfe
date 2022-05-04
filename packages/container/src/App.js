ce1c4fc1750714280551f104f406ae5abd353bc2
Caner <caner@favorisanat.com>
2022-05-04 13:05:48 +0300

    MFE-2 / Header added

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-    return (
-        <>
-        <h1>This is host Project</h1>
-        <MarketingApp />
-        </>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Header />
+      <MarketingApp />
+    </BrowserRouter>
+  );
+};
